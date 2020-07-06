6856dd88b1f064d1d6980751cd34a3b07d256f67
Mofizur Rahman <moficodes@gmail.com>
2020-07-06 02:31:37 -0400

    home page.

--- swiftuidemo/LandmarkList.swift
+++ swiftuidemo/LandmarkList.swift
@@ -12,32 +12,28 @@ struct LandmarkList: View {
     @EnvironmentObject private var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Show Favorites Only")
-                }
-                
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Show Favorites Only")
+            }
+            
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
 
 struct LandmarksList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+        NavigationView {
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+                .environmentObject(UserData())
         }
-        .environmentObject(UserData())
     }
 }
